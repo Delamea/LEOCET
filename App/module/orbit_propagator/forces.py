c74aebcb583a246fd6f968dedcb03d9d1befe566
Quentin DELAMEA <qdelamea@gmail.com>
2020-04-10 15:19:57 +0200

    Lunar gravity

--- App/module/orbit_propagator/forces.py
+++ App/module/orbit_propagator/forces.py
@@ -18,15 +18,19 @@ Compute the gravity force of a perfect spherical earth
 
 def lunar_gravity_force(position, lunar_position):
     """
-Compute the gravity force exerted by moon on the body
+Compute the gravity force exerted by Moon on the body
     :param position: vector body position
     :param lunar_position: vector lunar position
     :return: the force vector expressed in the geocentric frame
     """
-    return (-1*G*M_L)/(norm(position-lunar_position)**3)*(position-lunar_position)
+    return (-1 * G * M_L)/(norm(position - lunar_position) ** 3) * (position - lunar_position)
 
 
 def solar_gravity(position, solar_position):
-    return (-1 * G * M_S) / (norm(solar_position - position) ** 3) * (solar_position - position)
-
-
+    """
+Compute the gravity force exerted by Sun on the body
+    :param position: vector body position
+    :param solar_position: vector Solar position
+    :return: the force vector expressed in the geocentric frame
+    """
+    return (-1 * G * M_S) / (norm(solar_position - position) ** 3) * (position - solar_position)
